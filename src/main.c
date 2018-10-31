d43bd7f69ee48ee89e2939857908978ad2a34695
Oceane Flamant <oceane.flamant@isty.uvsq.fr>
2018-10-31 18:20:34 +0100

    Ajout de tri_parallele (non fonctionnelle + Modificztion minime sur les 2 fonctions précédentes.
    Les fonctions tri et tri_merge fonctionnent correctement.
    J'ai essayé de faire l'algo global mais le résultat n'est pas le bon et je sais pas si c'est parce que j'ai mal compris l'algo de base et donc je l'ai mal implémenté ou si s'est l'algo de base qui est faux.

--- src/main.c
+++ src/main.c
@@ -14,10 +14,12 @@ static int K;
 void tri(double *tab, int size);
 void generator(double tab[], int size);
 void tri_merge(double *tab1, double *tab2, int size);
+void tri_parallele(int N, int K, double blocks[N][K]);
 
 int main(int argc, char *argv[])
 {
-    /* Initialize parameters of the algorithm. */
+   
+   /* Initialize parameters of the algorithm. */
     if (argc <= 2)
         return fprintf(stderr, "Usage: %s [NUMBER OF BLOCKS] [SIZE OF ONE BLOCK]\n", argv[0]), 1;
     if ((N = atoi(argv[1])) < 1 || (K = atoi(argv[2])) < 1)
@@ -26,15 +28,22 @@ int main(int argc, char *argv[])
 
     /* Database to sort: N tables of K random elements each. */
     double blocks[N][K];
-    for (int i = 0; i < N; i++)
-        generator(blocks[i], K);
-        
     
     for (int i = 0; i < N; i++)
-        tri(blocks[i],K);
-  
-    tri_merge(blocks[0],blocks[1],K);
+            generator(blocks[i], K);
     
+  //  #pragma omp parallel for    
+        for (int i = 0; i<N; i++)
+            tri(blocks[i],K);
+    
+
+   tri_parallele(N, K, blocks);
+   
+   for(int i=0;i<N;i++)
+   {
+       for(int j=0;j<K;j++)
+        printf("%f\n", blocks[i][j]);
+    }
     
     return 0;
 }
@@ -55,13 +64,13 @@ void generator(double tab[], int size)
  */
 void tri(double *tab, int size)
 {
-    int i,j=0, test=1;
-    double tmp;
+    int j=1, test=1;
+    double tmp=0;
     
     while(test)
     {
         test=0;
-        for(i=0;i<size-j;i++)
+        for(int i=0;i<size-j;i++)
         {
             if(tab[i+1]<tab[i])
             {
@@ -71,6 +80,7 @@ void tri(double *tab, int size)
                 test=1;
             }
          }
+         j++;
       }
 }
 
@@ -82,13 +92,11 @@ void tri_merge(double *tab1, double *tab2, int size)
 {
     double tmp1[size], tmp2[size];
     int count1=0, count2=0;
-   
     for(int i=0;i<size;i++)
     {
         tmp1[i]=tab1[i];
         tmp2[i]=tab2[i];
     }
-    
     for(int k=0;k<size;k++)
     {
         if( (tmp1[count1]<tmp2[count2] && count1<size) || count2>=size) 
@@ -102,7 +110,6 @@ void tri_merge(double *tab1, double *tab2, int size)
             count2++;
         }
     }
-    
     for(int k=0;k<size;k++)
     {
         if( (tmp1[count1]<tmp2[count2] && count1<size) || count2>=size ) 
@@ -116,7 +123,43 @@ void tri_merge(double *tab1, double *tab2, int size)
             count2++;
         }
     }
-    
-    
 }
 
+void tri_parallele(int N, int K, double blocks[N][K])
+{
+     int k,b1,b2,min,max;
+   // #pragma omp parallel for
+        for (int i = 0; i < N; i++)
+            generator(blocks[i], K);
+    
+  //  #pragma omp parallel for    
+        for (int i = 0; i<N; i++)
+            tri(blocks[i],K);
+  
+     
+    
+    for(int j=1;j<N;j++)
+    {
+        k=1+(j%2);
+        for(int i=0;i<N/2;i++)
+        {
+            b1=(1+(k+2*i))%N;
+            b2=(1+(K+2*i+1))%N;
+            if(b1!=b2) //on peut pas merge 2 blocks si c'est le meme
+            {    
+                if(b1<b2)
+                {
+                    min=b1;
+                    max=b2;
+                }
+                else
+                {
+                    min=b2;
+                    max=b1;
+                }
+                tri_merge(blocks[min],blocks[max],K);
+                
+            }
+        }
+    }
+}
